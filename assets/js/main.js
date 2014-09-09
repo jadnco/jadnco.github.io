bb3532db76bb7c167ad02eb556daa36114041f75
Jaden Dessureault <jaden.dessureault@gmail.com>
2014-09-09 18:18:33 -0500

    Updates

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,3 +7,25 @@ $("#outline").on("click", function() {
     $(this).addClass("on");
   }
 });
+
+$("a.internal").on("click", function(e) {
+  e.preventDefault();
+
+  // Scroll to section in href
+  $("html, body").animate({
+    scrollTop: $($(this).attr("href")).offset().top
+  }, 800);
+});
+
+$(window).on("scroll", function() {
+  $("#container .sec").each(function() {
+    // Get scroll offset for section w/ 600px buffer
+    var scrollForSec = $(this).offset().top - 600;
+
+    if ($(window).scrollTop() >= scrollForSec) {
+      if (!$(this).find(".inner").hasClass("scrolled")) {
+        $(this).find(".inner").addClass("fade-in scrolled");
+      }
+    }
+  });
+});
