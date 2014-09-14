c0a00c1b2b1a697881c25163b585925f277fb4d3
Jaden Dessureault <jaden.dessureault@gmail.com>
2014-09-14 17:05:14 -0500

    Fix a majority of the bugs

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,22 +1,3 @@
-$("#outline").on("click", function() {
-  if ($(this).hasClass("on")) {
-    $("body").removeClass("outlined");
-    $(this).removeClass("on");
-  } else {
-    $("body").addClass("outlined");
-    $(this).addClass("on");
-  }
-});
-
-$("a.internal").on("click", function(e) {
-  e.preventDefault();
-
-  // Scroll to section in href
-  $("html, body").animate({
-    scrollTop: $($(this).attr("href")).offset().top
-  }, 800);
-});
-
 $(window).on("scroll", function() {
   $("#container .sec").each(function() {
     // Get scroll offset for section w/ 600px buffer
